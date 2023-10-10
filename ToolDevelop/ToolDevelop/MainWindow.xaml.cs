6a003fede1d0f7e8d1ccacf7fa0eedba8a0ad437
ENKO1995 <86885507+ENKO1995@users.noreply.github.com>
2023-10-10 16:25:15 +0200

    Add files via upload

---
+++ ToolDevelop/ToolDevelop/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ToolDevelop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void OnCloseMenu_Item(object _sender, RoutedEventArgs e)
+        {
+            Close(); // Close Window
+            Application.Current.Shutdown(); //Shutdown whole application
+        }
+    }
+}
